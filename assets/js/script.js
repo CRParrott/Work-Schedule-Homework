6c10d2f817de7a7dd6eaa528c13ff77a16cd6b57
Charlie Parrott <cparrott5700@gmail.com>
2021-05-01 17:59:38 -0400

    Finished table with time, text input, and button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
 var timeDisplayEl = $('#time-display');
+var timeColor = $('#col-task');
 
 // handle displaying the time
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm a');
+    var rightNow = moment().format('MMM DD, YYYY');
     timeDisplayEl.text(rightNow);
   }
+
+displayTime();
+
+// background color based on time
+function timeBackground() {
+}
+
+timeBackground();
+
+// save an entry to the form
+
